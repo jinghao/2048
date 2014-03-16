1c1be92e34f49ccf422ba7afcd6f76311a0ef636
Tricia <totricia@gmail.com>
2014-03-16 00:36:46 -0700

    Various GameTransitions tools
    GameTransitions.insertTile, isMoveInvalid, getEmptyCells

--- js/GameTransitions.js
+++ js/GameTransitions.js
@@ -1,4 +1,7 @@
 var Directions = require('./Directions');
+var Grid = require('./Grid');  // Merely to get some constants.
+
+var GRID_SIZE = 4;  // TO-DO: Put this somewhere meaningful.
 
 var GameTransitions = {
   move: function(currentState, direction) {
@@ -35,10 +38,37 @@ var GameTransitions = {
 
     for (var x = startX; x != endX; x += dx) {
       for (var y = startY; y != endY; y += dy) {
-        
+
       }
     }
 
+    return newState;
+  },
+
+  // Insert a tile at position x, y
+  insertTile: function(state, val, x, y) {
+    if (val <= 0 || val > Grid.MAX_CELL_VAL - 1) {
+      throw 'Cannot insert tile with value ' + val;
+    }
+
+    if (GameTransitions.getValue(state, x, y) != 0) {
+      throw 'Tile ' + GameTransitions.getValue(state, x, y) +
+        ' already in position ' + x + ', ' + y +
+        '; can\'t insert tile ' + val;
+    }
+
+    var offset = y * GRID_SIZE + x;
+    var newState;
+
+    if (offset >= Grid.CELLS_PER_STATE) {
+      newState = [state[0] +
+                    (val * Math.pow(Grid.MAX_CELL_VAL,
+                                    offset - Grid.CELLS_PER_STATE)),
+                  state[1]];
+    } else {
+      newState = [state[0], state[1] +
+                    (val * Math.pow(Grid.MAX_CELL_VAL, offset))];
+    }
     return newState;
   },
 
@@ -46,6 +76,27 @@ var GameTransitions = {
     var offset = (ypos * 4 + xpos);
 
     return (state[offset >= 8 ? 0 : 1] >> (offset * 4)) & 0xF;
+  },
+
+  // A move is invalid if the state after moving is the same as
+  // before moving
+  isMoveInvalid: function(state, move) {
+    return (GameTransitions.move(state, move)[0] == state[0] &&
+      GameTransitions.move(state, move)[1] == state[1]);
+  },
+
+  // Return the positions of cells that are empty
+  // ie: [[x1, y1], [x2, y2]]
+  getEmptyCells: function(state) {
+    var emptyCells = [];
+    for (var x = 0; x < GRID_SIZE; x++) {
+      for (var y = 0; y < GRID_SIZE; y++) {
+        if (GameTransitions.getValue(state, x, y) == 0) {
+          emptyCells.push([x, y]);
+        }
+      }
+    }
+    return emptyCells;
   }
 };
 
--- js/__tests__/GameTransitionsTest.js
+++ js/__tests__/GameTransitionsTest.js
@@ -18,6 +18,69 @@ describe('move', function() {
   });
 });
 
+describe('insertTile', function() {
+  it('should throw an exception if the value is too small',
+     function() {
+      expect(function() {
+        GameTransitions.insertTile([0x00000000, 0x00000000], 0, 0, 0);
+      }).toThrow();
+     });
+  it('should throw an exception if the value is too big',
+     function() {
+      expect(function() {
+        GameTransitions.insertTile([0x00000000, 0x00000000], 16, 0, 0);
+      }).toThrow();
+     });
+  it('should throw an exception if there is already a tile at the position',
+    function() {
+      expect(function() {
+        GameTransitions.insertTile([0x00000000, 0x00000001], 2, 0, 0);
+      }).toThrow();
+    });
+
+  [
+    {
+      state: [0x00000000, 0x00000000],
+      val: 1,
+      xpos: 0,
+      ypos: 0,
+      expectedState: [0x00000000, 0x00000001]
+    },
+    {
+      state: [0x00000000, 0x00000000],
+      val: 1,
+      xpos: 3,
+      ypos: 3,
+      expectedState: [0x10000000, 0x00000000]
+    },
+    {
+      state: [0x00000000, 0x00000000],
+      val: 1,
+      xpos: 0,
+      ypos: 0,
+      expectedState: [0x00000000, 0x00000001]
+    },
+    {
+      state: [0x00000000, 0x00000000],
+      val: 15,
+      xpos: 0,
+      ypos: 0,
+      expectedState: [0x00000000, 0x0000000f]
+    }
+  ].forEach(function(testInput) {
+    it('tests ' + testInput.state[0].toString(16) + ' ' +
+       testInput.state[1].toString(16) + ' ' + testInput.val,
+       function() {
+        expect(GameTransitions.insertTile(
+          testInput.state,
+          testInput.val,
+          testInput.xpos,
+          testInput.ypos
+        )).toEqual(testInput.expectedState);
+       });
+  })
+})
+
 describe('getValue', function() {
   [
     {
@@ -57,6 +120,70 @@ describe('getValue', function() {
         testInput.xpos,
         testInput.ypos
       )).toEqual(testInput.expected);
+    });
+  });
+});
+
+describe('isMoveInvalid', function() {
+  [
+    {
+      desc: '0x00000000, 0x00000001, LEFT',
+      state: [0x00000000, 0x00000001],
+      move: Directions.LEFT,
+      expected: true
+    },
+    {
+      desc: '0x00000000, 0x00000001, UP',
+      state: [0x00000000, 0x00000001],
+      move: Directions.UP,
+      expected: false
+    },
+    {
+      desc: '0x00000000, 0x00010001, LEFT',
+      state: [0x00000000, 0x00010001],
+      move: Directions.LEFT,
+      expected: false
+    }
+  ].forEach(function(testInput) {
+    xit('test: ' + testInput.desc, function() {
+      // Test is pending completion of GameTransitions.move
+      expect(GameTransitions.isMoveInvalid(
+        testInput.state,
+        testInput.move
+      )).toEqual(testInput.expected);
+    });
+  });
+});
+
+describe('getEmptyCells', function() {
+  var allCells = [];
+  for (var x = 0; x < 4; x++) {
+    for (var y = 0; y < 4; y++) {
+      allCells.push([x, y]);
+    }
+  }
+
+  [
+    {
+      desc: '0x00000000, 0x00000000',
+      state: [0x00000000, 0x00000000],
+      expected: allCells
+    },
+    {
+      desc: '0x12345678, 0x12345678',
+      state: [0x12345678, 0x12345678],
+      expected: []
+    },
+    {
+      desc: '0x12305678, 0x12345678',
+      state: [0x12305678, 0x12345678],
+      expected: [[0, 3]]
+    }
+  ].forEach(function(testInput) {
+    it('test: ' + testInput.desc, function() {
+      expect(GameTransitions.getEmptyCells(
+        testInput.state
+      ).sort()).toEqual(testInput.expected.sort());
     });
   });
 });
